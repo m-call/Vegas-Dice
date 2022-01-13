b14f3dba4e7ef001a2d287be12b85a5fd2e40a2c
Michael Callahan <mcallahan0@yahoo.com>
2022-01-13 01:10:46 -0500

    Created immutable array diceArr for all 6 dice images. Used randomElement method on the diceArr inside the IBAction to change each dice's image value when the Roll button is pressed.

--- Vegas Dice/ViewController.swift
+++ Vegas Dice/ViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+
+    let diceArr = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Used #imageLiteral( to choose image
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = diceArr.randomElement()
+        diceImageView2.image = diceArr.randomElement()
     }
-
-
+    
 }
 
