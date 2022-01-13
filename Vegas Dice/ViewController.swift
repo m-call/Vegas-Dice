c2817b4f58a32e60ce19e3c7c3c41ea255b45a3a
Michael Callahan <mcallahan0@yahoo.com>
2022-01-13 00:12:04 -0500

    Set up IBOutlet for both dice in ViewController.swift and set the image for each dice in viewDidLoad

--- Vegas Dice/ViewController.swift
+++ Vegas Dice/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Used #imageLiteral( to choose image
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
